028e558c50fd526c0172ff4495fd878e2733abfc
trovtle basnet <basnet.trovtle@gmail.com>
2021-04-18 15:32:15 +1000

    Slider optimized

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,9 +7,11 @@ import Message from './Message';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { useHistory } from 'react-router';
 
 const CarouselContainer = () => {
 	const dispatch = useDispatch();
+	const history = useHistory();
 
 	const productTopRated = useSelector((state) => state.productTopRated);
 	const { loading, error, products } = productTopRated;
@@ -25,14 +27,18 @@ const CarouselContainer = () => {
 	) : (
 		<Carousel autoFocus autoPlay emulateTouch className="carouselComp">
 			{products.map((product) => (
-				<div className="carouselComp__item" key={product._id}>
-					<img src={product.image} alt={product.name} />
-					<div className="carouselComp__nameButton">
+				<div
+					className="carouselComp__item"
+					key={product._id}
+					onClick={() => history.push(`/product/${product._id}`)}>
+					<img
+						src={product.image}
+						alt={product.name}
+						className="carouselComp__itemImage"
+					/>
+					<div className="carouselComp__bottom">
 						<p>{product.name}</p>
 						<p>${product.price}</p>
-						<button className="carouselComp__button">
-							Buy Now
-						</button>
 					</div>
 				</div>
 			))}
